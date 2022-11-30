3891c93d467bb3af3b1f52d79584a8e98cf6efcf
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-30 12:53:51 -0500

    created restaurants and events api in componenets

---
+++ client/src/components/events.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Zipcode from "./eventsapi";
+
+
+
+export default function Events() {
+return (
+<div>
+{/* this creates and populates the api with the proper information for the website */}
+<h1>EVENTS</h1>
+<button onClick = { Zipcode("30047") }>Click Here</button>
+
+
+
+
+
+</div>
+
+)
+}
---
+++ client/src/components/restaurants.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Zipcode from "./restaurantapi";
+
+
+
+export default function Rests() {
+return (
+<div>
+{/* this creates and populates the api with the proper information for the website */}
+<h1>RESTAURANTS</h1>
+<button onClick = { Zipcode("30047") }>Click Here</button>
+
+
+
+
+
+</div>
+
+)
+}
