ccbef102aa9f52034173299b3ae4ab5366beb9b9
ALAN JOSE <alanjose04231120@gmail.com>
2022-12-01 14:14:29 -0500

    created default app

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
-import Home from './components/userpage';
+import Home from './components/Main/userpage';
 import About from './components/restaurants';
 import Contact from './components/restaurants';
 import './App.css';
   
-class App extends Component {
-  render() {
+function App () {
+  
     return (
-       <Router>
-           <div className="App">
-            <ul className="App-header">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact Us</Link>
-              </li>
-            </ul>
-           <Routes>
-                 <Route exact path='/' element={< Home />}></Route>
-                 <Route exact path='/about' element={< About />}></Route>
-                 <Route exact path='/contact' element={< Contact />}></Route>
-          </Routes>
-          </div>
-       </Router>
-   );
-  }
+      <div>
+
+        
+      </div>
+    )
+  
 }
   
 export default App;
