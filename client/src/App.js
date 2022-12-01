b311b67a1bbeddae40153b7321d6c76067d165df
ALAN JOSE <alanjose04231120@gmail.com>
2022-12-01 13:36:10 -0500

    added components and models styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import Home from './components/userpage';
+import About from './components/restaurants';
+import Contact from './components/restaurants';
 import './App.css';
-import Events from "./components/events";
-
-function App() {
-  return  (
-    <div className="App">
-      <Events/>
-    </div>
-  );
+  
+class App extends Component {
+  render() {
+    return (
+       <Router>
+           <div className="App">
+            <ul className="App-header">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+            </ul>
+           <Routes>
+                 <Route exact path='/' element={< Home />}></Route>
+                 <Route exact path='/about' element={< About />}></Route>
+                 <Route exact path='/contact' element={< Contact />}></Route>
+          </Routes>
+          </div>
+       </Router>
+   );
+  }
 }
-
+  
 export default App;
